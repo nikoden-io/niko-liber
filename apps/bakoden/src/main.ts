639a7c44c76b2cd43d65458d5aad2f8c492a8307
Nicolas Denoël <contact@nikoden.io>
2023-03-01 21:50:28 +0100

    [bakoden] Fastify initial setup and demo routes linked with PokéAPI

--- apps/bakoden/src/main.ts
+++ apps/bakoden/src/main.ts
@@ -1,23 +1,11 @@
 import Fastify from 'fastify';
 import { app } from './app/app';
+import { startServer } from './server-config/start-server';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
-
-// Instantiate Fastify with some config
-const server = Fastify({
+const webServer = Fastify({
   logger: true,
 });
 
-// Register your application as a normal plugin.
-server.register(app);
+webServer.register(app);
 
-// Start listening.
-server.listen({ port, host }, (err) => {
-  if (err) {
-    server.log.error(err);
-    process.exit(1);
-  } else {
-    console.log(`[ ready ] http://${host}:${port}`);
-  }
-});
+void startServer(webServer);
